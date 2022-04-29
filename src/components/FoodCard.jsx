045477778c7ae0f48fdc31d186bb09a72d5175ba
Victor Caruso <vic.caruso@gmail.com>
2022-04-29 10:43:50 -0700

    Add filter/search state and logic to FoodList component

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React from 'react';
 
 export default function FoodCard({ food }) {
   return (
-    <div className='food-card'>
+    <div className="food-card">
       <h3>{food.description}</h3>
-      {
-        food.foodNutrients.map(nutrient => <p>{`${nutrient.name}: ${nutrient.amount} ${nutrient.unitName}`}</p>)
-      }
+      {food.foodNutrients.map((nutrient, index) => (
+        <p
+          key={index}
+        >{`${nutrient.name}: ${nutrient.amount} ${nutrient.unitName}`}</p>
+      ))}
     </div>
-  )
+  );
 }
--- src/views/FoodList.jsx
+++ src/views/FoodList.jsx
@@ -6,6 +6,14 @@ import { fetchFoods } from '../services/foodapi';
 export default function FoodList() {
   const [loading, setLoading] = useState(false);
   const [foods, setFoods] = useState([]);
+  const [filteredFoods, setFilteredFoods] = useState([]);
+
+  const handleSearch = (search) => {
+    const matches = foods.filter((food) =>
+      food.description.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredFoods(matches);
+  };
 
   useEffect(() => {
     async function fetch() {
@@ -17,20 +25,21 @@ export default function FoodList() {
 
     fetch();
   }, []);
-  return (
-    <>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Filter />
-          <div className="food-list">
-            {foods.map((food) => (
-              <FoodCard key={food.fdcId} food={food} />
-            ))}
-          </div>
-        </>
-      )}
-    </>
-  );
+
+  if (loading) {
+    return <p>Loading...</p>;
+  } else {
+    return (
+      <>
+        <Filter onSearch={handleSearch} />
+        <div className="food-list">
+          {filteredFoods.length
+            ? filteredFoods.map((food) => (
+                <FoodCard key={food.fdcId} food={food} />
+              ))
+            : foods.map((food) => <FoodCard key={food.fdcId} food={food} />)}
+        </div>
+      </>
+    );
+  }
 }
